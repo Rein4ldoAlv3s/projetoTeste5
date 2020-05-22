028e4380214c39fd642efd40c35ecc75ccc2a340
reinaldo <reinaldo@reinaldo-desktop>
2020-05-22 20:02:32 -0300

    Primeiro commit (Reinaldo)

--- src/projetointegrador5/ProjetoIntegrador5.java
+++ src/projetointegrador5/ProjetoIntegrador5.java
@@ -15,7 +15,7 @@ public class ProjetoIntegrador5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("dsfsdfdsfsd");
+        System.out.println("Olá, Mundo -Reinaldo");
     }
     
 }
