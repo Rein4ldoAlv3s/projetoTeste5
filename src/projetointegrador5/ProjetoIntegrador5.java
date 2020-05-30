3dfd27d97f5305a12acc0d9b1fcb9c27db90df85
VitorNery <61890856+VitorNery@users.noreply.github.com>
2020-05-29 22:03:01 -0300

    suggestion of update

--- src/projetointegrador5/ProjetoIntegrador5.java
+++ src/projetointegrador5/ProjetoIntegrador5.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.util.Scanner;
 package projetointegrador5;
 
 /**
@@ -15,7 +16,16 @@ public class ProjetoIntegrador5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner read = new Scanner (System.in);
         System.out.println("Olá, Mundo -Reinaldo");
+        
+        System.out.println("Enter a number:");
+        int num = read.nextInt();
+        
+        if(num>0 && num<3){
+            System.out.println("You won!!");
+        }else{
+            System.out.println("You lost!!");
+        }  
     }
-    
 }
